4375e898293e95321b9d416230290312d1119303
dgumenyuk <log6305.poly@gmail.com>
2024-03-15 07:50:27 -0400

    Adding the examples

--- poly_llm/generators/llm_test_generator.py
+++ poly_llm/generators/llm_test_generator.py
@@ -14,20 +14,46 @@ class LLMTestGenerator(AbstractGenerator):
         self._func_name = function.__name__
 
     def generate_assertions(self, prompt:str):
+        """
+        Generates assertions based on the given prompt.
+
+        Args:
+            prompt (str): The input prompt for generating assertions.
+
+        Returns:
+            str: The generated assertions as a string.
+        """
         input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
         generated_ids = self.model.generate(input_ids, max_length=512)
         output_text = self.tokenizer.decode(generated_ids[0], skip_special_tokens=True)
         return output_text
 
     def parse_assertions(self, generated_text):
-        """Parse the generated text and extract lines that contain assertions."""
+        """
+        Parse the generated text and extract lines that contain assertions.
+
+        Args:
+            generated_text (str): The generated text to parse.
+
+        Returns:
+            list: A list of lines that contain assertions.
+        """
         lines = generated_text.split("\n")
         assertions = [
             line.strip() for line in lines if line.strip().startswith("assert")
         ]
         return assertions
 
-    def create_test_function(self,  code_snippet):
+    def create_test_function(self, code_snippet):
+        """
+        Creates a test function for the given code snippet.
+
+        Args:
+            code_snippet (str): The code snippet to generate assertions for.
+
+        Returns:
+            tuple: A tuple containing the generated test function code and the test function name.
+        """
         function_name = self._func_name
         # Generate text that likely includes assertions
         generated_text = self.generate_assertions(code_snippet)
@@ -49,6 +75,14 @@ class LLMTestGenerator(AbstractGenerator):
         return test_function_code, f"test_{function_name}"
 
     def write_test_to_file(self, test_function_code, filename="test_generated.py"):
+        """
+        Writes the test function code to a file.
+
+        Args:
+            test_function_code (str): The code of the test function to be written.
+            filename (str, optional): The name of the file to write the test function code to. 
+                Defaults to "test_generated.py".
+        """
         with open(filename, "a") as file:
             file.write(test_function_code)
         print(f"Test function written to {filename}")
